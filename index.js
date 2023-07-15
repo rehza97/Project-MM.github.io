6d5c20923b40298b603e676e806f6dfefdf60931
Tebib Fethallah <fethotebib3@gmail.com>
2023-07-15 17:54:15 +0200

    the bug fixed

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict'
 const sideMenu = document.querySelector('aside');
 const menuBtn = document.getElementById('menu-btn');
 const closeBtn = document.getElementById('close-btn');
@@ -53,3 +54,70 @@ class Progbar{
  new Progbar(document.querySelector('.two'),4);
  new Progbar(document.querySelector('.three'),20);
  new Progbar(document.querySelector('.four'),54);
+
+
+ const next_id = document.querySelector('#next')
+ const prev_id = document.querySelector('#prev')
+ 
+ const next_class = document.querySelector('.next')
+ const prev_class = document.querySelector('.prev')
+
+ const newUsers = document.querySelector('.new-users') 
+
+
+
+next_class.addEventListener('mousemove', function(event) {
+    var rect = next_class.getBoundingClientRect();
+    var mouseX = event.clientX - rect.left;
+    var mouseY = event.clientY - rect.top;
+
+    if (mouseX >= 0 && mouseY >= 0 && mouseX <= rect.width && mouseY <= rect.height) {
+      console.log('Mouse is hovering over the target area');
+      next_id.style.transform = `translateX(-100%)`;
+      
+
+
+    }
+  });
+next_class.addEventListener('mouseout', function(event) {
+    var rect = next_class.getBoundingClientRect();
+    var mouseX = event.clientX - rect.left;
+    var mouseY = event.clientY - rect.top;
+
+    if (!(mouseX >= 0 && mouseY >= 0 && mouseX <= rect.width && mouseY <= rect.height)) {
+        console.log('Mouse is hovering outside the target area');
+        next_id.style.transform = `translateX(100%)`;
+        next_class.style.display = 'block';
+        prev_id.style.transform = `translateX(-100%)`;
+        prev_class.style.display = 'block';
+
+    }
+  });
+prev_class.addEventListener('mousemove', function(event) {
+    var rect = prev_class.getBoundingClientRect();
+    var mouseX = event.clientX - rect.left;
+    var mouseY = event.clientY - rect.top;
+
+    if (mouseX >= 0 && mouseY >= 0 && mouseX <= rect.width && mouseY <= rect.height) {
+      console.log('Mouse is hovering over the target area');
+      prev_id.style.transform = `translateX(100%)`;
+      next_id.style.transform = `translateX(100%)`;
+      
+
+
+    }
+  });
+prev_class.addEventListener('mouseout', function(event) {
+    var rect = prev_class.getBoundingClientRect();
+    var mouseX = event.clientX - rect.left;
+    var mouseY = event.clientY - rect.top;
+
+    if (!(mouseX >= 0 && mouseY >= 0 && mouseX <= rect.width && mouseY <= rect.height)) {
+        console.log('Mouse is hovering outside the target area');
+        prev_id.style.transform = `translateX(-100%)`;
+        prev_class.style.display = 'block';
+        next_id.style.transform = `translateX(100%)`;
+        next_class.style.display = 'block';
+
+    }
+  });
