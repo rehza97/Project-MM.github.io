d7c19affaf919e7a3fd8f96a3ada4dc5ddfae200
Tebib Fethallah <fethotebib3@gmail.com>
2023-07-16 19:56:59 +0200

    half work done

--- index.js
+++ index.js
@@ -128,3 +128,18 @@ prev_class.addEventListener("mouseout", function (event) {
     next_class.style.display = "block";
   }
 });
+
+const btnEl = document.querySelector(".btn");
+const toggleOptions = () => {
+  const wrapperEl = document.querySelector(".wrapper");
+  const iconEl = btnEl.querySelector("i");
+
+  wrapperEl.classList.toggle("active");
+
+  if (iconEl.classList.contains("ri-share-line")) {
+    iconEl.classList.replace("ri-share-line", "ri-close-line");
+  } else {
+    iconEl.classList.replace("ri-close-line", "ri-share-line");
+  }
+};
+btnEl.addEventListener("click", toggleOptions);
